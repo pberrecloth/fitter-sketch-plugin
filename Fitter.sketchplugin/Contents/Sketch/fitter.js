3f49c52550637d873e20ac28a8e954662b893096
Peter Berrecloth <pberrecloth@qumu.com>
2016-11-14 16:58:20 +0000

    Fit to Artboard Margin

--- Fitter.sketchplugin/Contents/Sketch/fitter.js
+++ Fitter.sketchplugin/Contents/Sketch/fitter.js
@@ -23,6 +23,37 @@ function fitWidthToArtboard(context) {
         return
     }
 };
+function fitToArtboardMargins(context) {
+    var doc = context.document
+    var selection = context.selection
+
+    if (selection.count() == 0) {
+        doc.showMessage("Please select a layer.")
+        return
+
+    } else if (selection.count() == 1) {
+        var layer = selection[0]
+        var artboard = doc.currentPage().currentArtboard() || doc.currentPage()
+
+        var margin = [[doc askForUserInput:"Margin" ofType:1 initialValue:"16"] integerValue];
+
+        var newX = 0 + margin
+        var newWidth = artboard.frame().width() - margin*2
+
+
+        layer.frame().setX(newX)
+        // layer.frame().setY(0)
+        layer.frame().setWidth(newWidth)
+        // layer.frame().setHeight(artboard.frame().height())
+
+        doc.currentPage().deselectAllLayers()
+        layer.select_byExpandingSelection(true, true)
+
+    } else if (selection.count() > 1) {
+        doc.showMessage("Please select one layer.")
+        return
+    }
+};
 function fitHeightToArtboard(context) {
     var doc = context.document
     var selection = context.selection
