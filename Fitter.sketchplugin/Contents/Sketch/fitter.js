cf151c88150684e4e2d0ec7341c3c7925ce0eae1
Peter Berrecloth <peter.berrecloth@qumu.com>
2016-11-15 13:26:32 +0000

    Fitting for child layers and across multiple artboards

--- Fitter.sketchplugin/Contents/Sketch/fitter.js
+++ Fitter.sketchplugin/Contents/Sketch/fitter.js
@@ -1,106 +1,81 @@
-function fitWidthToArtboard(context) {
-    var doc = context.document
-    var selection = context.selection
 
-    if (selection.count() == 0) {
-        doc.showMessage("Please select a layer.")
-        return
-
-    } else if (selection.count() == 1) {
-        var layer = selection[0]
-        var artboard = doc.currentPage().currentArtboard() || doc.currentPage()
-
-        layer.frame().setX(0)
-        // layer.frame().setY(0)
-        layer.frame().setWidth(artboard.frame().width())
-        // layer.frame().setHeight(artboard.frame().height())
-
-        doc.currentPage().deselectAllLayers()
-        layer.select_byExpandingSelection(true, true)
-
-    } else if (selection.count() > 1) {
-        doc.showMessage("Please select one layer.")
-        return
-    }
-};
-function fitToArtboardMargins(context) {
-    var doc = context.document
-    var selection = context.selection
-
-    if (selection.count() == 0) {
-        doc.showMessage("Please select a layer.")
-        return
-
-    } else if (selection.count() == 1) {
-        var layer = selection[0]
-        var artboard = doc.currentPage().currentArtboard() || doc.currentPage()
-
-        var margin = [[doc askForUserInput:"Margin" ofType:1 initialValue:"16"] integerValue];
-
-        var newX = 0 + margin
-        var newWidth = artboard.frame().width() - margin*2
-
-
-        layer.frame().setX(newX)
-        // layer.frame().setY(0)
-        layer.frame().setWidth(newWidth)
-        // layer.frame().setHeight(artboard.frame().height())
-
-        doc.currentPage().deselectAllLayers()
-        layer.select_byExpandingSelection(true, true)
-
-    } else if (selection.count() > 1) {
-        doc.showMessage("Please select one layer.")
-        return
-    }
-};
-function fitHeightToArtboard(context) {
-    var doc = context.document
-    var selection = context.selection
-
-    if (selection.count() == 0) {
-        doc.showMessage("Please select a layer.")
-        return
-
-    } else if (selection.count() == 1) {
-        var layer = selection[0]
-        var artboard = doc.currentPage().currentArtboard() || doc.currentPage()
-
-        // layer.frame().setX(0)
-        layer.frame().setY(0)
-        // layer.frame().setWidth(artboard.frame().width())
-        layer.frame().setHeight(artboard.frame().height())
-
-        doc.currentPage().deselectAllLayers()
-        layer.select_byExpandingSelection(true, true)
-
-    } else if (selection.count() > 1) {
-        doc.showMessage("Please select one layer.")
-        return
-    }
-};
+// COMMANDS
 function fitToArtboard(context) {
-    var doc = context.document
-    var selection = context.selection
+  fitterProcessor(context, 0, 1, 1)
+};
+function fitToArtboardWidth(context) {
+  fitterProcessor(context, 0, 1, null)
+};
+function fitToArtboardHeight(context) {
+  fitterProcessor(context, 0, null, 1)
+};
+function fitToArtboardMargin(context) {
+  var doc = context.document
+  var margin = [[doc askForUserInput:"Margin" ofType:1 initialValue:"16"] integerValue];
+  fitterProcessor(context, margin, 1, 1)
+}
+function fitToArtboardWidthMargin(context) {
+  var doc = context.document
+  var margin = [[doc askForUserInput:"Margin" ofType:1 initialValue:"16"] integerValue];
+  fitterProcessor(context, margin, 1, null)
+}
+function fitToArtboardHeightMargin(context) {
+  var doc = context.document
+  var margin = [[doc askForUserInput:"Margin" ofType:1 initialValue:"16"] integerValue];
+  fitterProcessor(context, margin, null, 1)
+}
 
-    if (selection.count() == 0) {
-        doc.showMessage("Please select a layer.")
-        return
 
-    } else if (selection.count() == 1) {
-        var layer = selection[0]
-        var artboard = doc.currentPage().currentArtboard() || doc.currentPage()
+// PROCESSOR
+function fitterProcessor(context, margin, fitHorizontally, fitVertically) {
+  var doc = context.document
+  var selection = context.selection
+  var selectionCount = selection.count()
 
-        layer.frame().setX(0)
-        layer.frame().setY(0)
-        layer.frame().setWidth(artboard.frame().width())
-        layer.frame().setHeight(artboard.frame().height())
+  if (selectionCount == 0) {
+    doc.showMessage("Please select a layer.")
+    return
 
-        doc.currentPage().deselectAllLayers()
-        layer.select_byExpandingSelection(true, true)
+  } else if (selectionCount >= 1) {
 
-    } else if (selection.count() > 1) {
-        doc.showMessage("Please select one layer.")
-        return
+    for (i=0;i<=selectionCount;i++){
+      var layer = selection[i]
+
+      if ([layer class] != "MSArtboardGroup") {
+
+        var layerParentGroup = [layer parentGroup];
+
+        // Ensures the layer scales to the parent artboard
+        while (layerParentGroup){
+          if ([layerParentGroup class] == "MSArtboardGroup"){
+            artboardToSelect = layerParentGroup;
+            break;
+          }
+          // If layer is nested in any groups we must reset the position of the parent because child position is relative
+          if ([layerParentGroup class] == "MSLayerGroup"){
+            if (fitVertically){ var layerGroupY = layerParentGroup.frame().setY(0) }
+            if (fitHorizontally){ var layerGroupX = layerParentGroup.frame().setX(0) }
+          }
+          layerParentGroup = [layerParentGroup parentGroup];
+        }
+        var artboard = layerParentGroup
+
+        if (fitHorizontally){
+          var newX = margin
+          var newWidth = artboard.frame().width() - margin*2
+          layer.frame().setX(newX)
+          layer.frame().setWidth(newWidth)
+        }
+
+        if (fitVertically){
+          var newY = margin
+          var newHeight = artboard.frame().height() - margin*2
+          layer.frame().setY(newY)
+          layer.frame().setHeight(newHeight)
+        }
+      }
+
     }
+
+  }
 };
--- README.md
+++ README.md
@@ -1,3 +1,22 @@
-# fitter-sketch-plugin
+# Fitter Sketch Plugin
 
 Fits layer to artboard width, height or both.
+
+
+**Commands**
+
+![Example](screenshots/menu.png?raw=true "Example")
+
+
+## Keyboard Shortcuts
+
+Especially useful when assigned to a keyboard shortcut. You must do this yourself under _Apple > Preferences > Keyboard > Shortcuts_. [How to set up Keyboard Shortcuts](http://www.sketchtips.info/articles/custom-shortcuts).
+
+Commands to copy are:
+
+-
+
+
+## Installation
+
+[Install from Sketch Toolbox](http://sketchtoolbox.com/)
